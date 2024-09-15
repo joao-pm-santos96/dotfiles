a05b02897161b015d33aeb4b7502b19e6342c2b6
joao.pm.santos96 <joao@lumenrobotics.eu>
2024-09-15 10:54:30 +0100

    ...

--- run_onchange_before_install-packages.sh
+++ run_onchange_before_install-packages.sh
@@ -5,5 +5,7 @@ sudo apt install -y zsh git curl
 
 # Poetry
 curl -sSL https://install.python-poetry.org | python3 -
+
+[ ! -f "~/.zfunc/_poetry" ] && touch "~/.zfunc/_poetry"
 poetry completions zsh > ~/.zfunc/_poetry
 
