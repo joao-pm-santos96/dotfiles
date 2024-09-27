3974f621622e8e8c713521aa176d4a17f4d502cd
João Santos <47630528+joao-pm-santos96@users.noreply.github.com>
2024-09-27 11:50:27 +0100

    ...

--- run_onchange_before_install-packages.sh
+++ run_onchange_before_install-packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y zsh git curl age  python3-poetry
+sudo apt install -y zsh git curl tmux
 
 # Poetry
 curl -sSL https://install.python-poetry.org | python3 -
 
+chsh -s $(which zsh)
 mkdir -p ~/.zfunc
 poetry completions zsh > ~/.zfunc/_poetry
 
